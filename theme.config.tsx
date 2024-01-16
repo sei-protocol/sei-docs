b6b87e3cf9c0ffa05f61cbf39fbbb6c7d6c08d24
besated <bryan@seinetwork.io>
2024-01-16 15:17:39 -0800

    Add seo props

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,38 @@ const config: DocsThemeConfig = {
   footer: {
     text: "Sei Docs © 2024",
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Sei Docs",
+      description: "Documentation for Sei Network",
+      openGraph: {
+        type: "website",
+        locale: "en_US",
+        siteName: "Sei Docs",
+        description: "Documentation for Sei Network",
+      },
+      twitter: {
+        site: "@SeiNetwork",
+      },
+      // additionalLinkTags: [
+      //   {
+      //     rel: "icon",
+      //     href: "/favicon.ico",
+      //   },
+      //   {
+      //     href: "/favicon-96x96.png",
+      //     rel: "icon",
+      //     sizes: "96x96",
+      //     type: "image/png",
+      //   },
+      //   {
+      //     href: "/apple-icon-180x180.png",
+      //     rel: "apple-touch-icon",
+      //     sizes: "180x180",
+      //   },
+      // ],
+    };
+  },
 };
 
 export default config;
