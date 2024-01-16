611c2379e3f9144a26f8f570621115c64d846714
besated <bryan@seinetwork.io>
2024-01-16 15:08:01 -0800

    Add base page structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Sei</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/sei-protocol",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/sei",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/sei-protocol/sei-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Sei Docs © 2024",
   },
-}
+};
 
-export default config
+export default config;
