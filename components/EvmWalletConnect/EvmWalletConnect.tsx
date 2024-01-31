7d14f8371e0096e6b4769808d6f9049a53f203cd
besated <bryan@seinetwork.io>
2024-01-30 16:27:53 -0800

    Update default rpc urls

--- components/EvmWalletConnect/EvmWalletConnect.tsx
+++ components/EvmWalletConnect/EvmWalletConnect.tsx
@@ -8,8 +8,13 @@ import { Chain, configureChains, createConfig, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import styles from "./EvmWalletConnect.module.css";
 import "@rainbow-me/rainbowkit/styles.css";
+import { ChainRpcUrls } from "viem/_types/types/chain";
 
 export default function EvmWalletConnect() {
+  const rpcUrl: ChainRpcUrls = {
+    http: ["https://evm-rpc-arctic-1.sei-apis.com"],
+    webSocket: ["wss://evm-ws-arctic-1.sei-apis.com"],
+  };
   const seiDevnet: Chain = {
     id: 713715,
     name: "Sei EVM Devnet",
@@ -20,8 +25,12 @@ export default function EvmWalletConnect() {
       symbol: "SEI",
     },
     rpcUrls: {
-      public: { http: ["https://evm-rpc.arctic-1.seinetwork.io/"] },
-      default: { http: ["https://evm-rpc.arctic-1.seinetwork.io/"] },
+      public: rpcUrl,
+      default: rpcUrl,
+    },
+    testnet: true,
+    blockExplorers: {
+      default: { name: "Seistream", url: "https://seistream.app/" },
     },
   };
 
