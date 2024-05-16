9e8c915729ce00e3831c873eb92b8b398f66ef78
cordt-sei <165932662+cordt-sei@users.noreply.github.com>
2024-05-15 22:48:40 -0600

    Update EvmWalletConnect **L42 incomplete**
    # update metamask connect button for mainnet 
    
    line 42 `const projectId = "???";` needs attention before merging

--- components/EvmWalletConnect/EvmWalletConnect.tsx
+++ components/EvmWalletConnect/EvmWalletConnect.tsx
@@ -1,7 +1,7 @@
 import {
-  ConnectButton,
-  connectorsForWallets,
-  RainbowKitProvider,
+    ConnectButton,
+    connectorsForWallets,
+    RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
 import { injectedWallet, metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
 import { Chain, configureChains, createConfig, WagmiConfig } from "wagmi";
@@ -10,62 +10,63 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { ChainRpcUrls } from "viem/_types/types/chain";
 
 export default function EvmWalletConnect() {
-  const rpcUrl: ChainRpcUrls = {
-    http: ["https://evm-rpc-arctic-1.sei-apis.com"],
-    webSocket: ["wss://evm-ws-arctic-1.sei-apis.com"],
-  };
-  const seiDevnet: Chain = {
-    id: 713715,
-    name: "Sei EVM Devnet",
-    network: "sei",
-    nativeCurrency: {
-      decimals: 18,
-      name: "Sei",
-      symbol: "SEI",
-    },
-    rpcUrls: {
-      public: rpcUrl,
-      default: rpcUrl,
-    },
-    testnet: true,
-    blockExplorers: {
-      default: { name: "Seistream", url: "https://seistream.app/" },
-    },
-  };
+    const rpcUrl: ChainRpcUrls = {
+        http: ["https://evm-rpc.sei-apis.com"],
+        webSocket: ["wss://evm-ws.sei-apis.com"],
+    };
+    const seiDevnet: Chain = {
+        id: 513,
+        name: "Sei Network",
+        network: "Sei",
+        nativeCurrency: {
+            decimals: 18,
+            name: "Sei",
+            symbol: "SEI",
+        },
+        rpcUrls: {
+            public: rpcUrl,
+            default: rpcUrl,
+        },
+        testnet: true,
+        blockExplorers: {
+            default: { name: "Seitrace", url: "https://seitrace.com" },
+        },
+    };
 
-  const { chains, publicClient } = configureChains(
-    [seiDevnet],
-    [publicProvider()]
-  );
+    const { chains, publicClient } = configureChains(
+        [seiDevnet],
+        [publicProvider()]
+    );
 
-  const projectId = "385413c214cb74213e0679bc30dd4e4c";
-  const connectors = connectorsForWallets([
-    {
-      groupName: "Recommended",
-      wallets: [
-        injectedWallet({ chains }),
-        metaMaskWallet({ projectId, chains }),
-      ],
-    },
-  ]);
+    // This needs to be updated
+    const projectId = "???";
+    const connectors = connectorsForWallets([
+        {
+            groupName: "Recommended",
+            wallets: [
+                injectedWallet({ chains }),
+                metaMaskWallet({ projectId, chains }),
+            ],
+        },
+    ]);
 
-  const wagmiConfig = createConfig({
-    autoConnect: false,
-    connectors,
-    publicClient,
-  });
+    const wagmiConfig = createConfig({
+        autoConnect: false,
+        connectors,
+        publicClient,
+    });
 
-  return (
-    <div className="my-4 flex justify-center">
-      <WagmiConfig config={wagmiConfig}>
-        <RainbowKitProvider chains={chains}>
-          <ConnectButton
-            accountStatus="address"
-            chainStatus="icon"
-            showBalance={false}
-          />
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </div>
-  );
+    return (
+        <div className="my-4 flex justify-center">
+            <WagmiConfig config={wagmiConfig}>
+                <RainbowKitProvider chains={chains}>
+                    <ConnectButton
+                        accountStatus="address"
+                        chainStatus="icon"
+                        showBalance={false}
+                    />
+                </RainbowKitProvider>
+            </WagmiConfig>
+        </div>
+    );
 }
