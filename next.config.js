4b94aeffaf1ef4614c20880fd030427e92ed0a29
cordt-sei <cordt@seifdn.org>
2024-05-25 23:32:29 -0600

    Adds brand-kit, CSS + component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  images: {
+    domains: ['cdn.sei.io'],
+  },
+});
