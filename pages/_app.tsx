2f94847021e9b8f257a4013c4c41d629ae37451c
Carson <104383295+codebycarson@users.noreply.github.com>
2024-06-03 09:31:06 -0700

    Improved the structure and content of many sections within the for devs section of the docs.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,18 @@
 import '../styles/globals.css';
 import '../styles/custom.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { ThemeProvider } from 'next-themes';
+import {ThemeProvider} from 'next-themes';
 import '@mantine/core/styles.css';
-import { createTheme, MantineProvider } from '@mantine/core';
+import {MantineWrapper} from "../components/MantineWrapper";
 
-const theme = createTheme({
-/** Put your mantine theme override here */
-});
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-      <MantineProvider theme={theme}>
-          <ThemeProvider attribute="class">
-              <Component {...pageProps} />
-          </ThemeProvider>
-      </MantineProvider>
+    return (
+        <ThemeProvider attribute="class">
+            <MantineWrapper>
+                <Component {...pageProps} />
+            </MantineWrapper>
+        </ThemeProvider>
 
   );
 }
