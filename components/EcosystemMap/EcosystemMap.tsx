a4551facfacb366b68a32140196eab06d748d03f
partyparrotgreg <2233348+partyparrotgreg@users.noreply.github.com>
2024-06-06 19:44:47 +0100

    simple ecosystem map

---
+++ components/EcosystemMap/EcosystemMap.tsx
@@ -0,0 +1,77 @@
+import { ExternalLinkIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { groupBy } from 'underscore'
+import {
+  EcosystemResponse,
+  getSeiEcosystemAppsData,
+} from '../../data/ecosystemData'
+
+const EcosystemMap = () => {
+  const [apps, setApps] = useState<EcosystemResponse['data'] | null>(null)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const fetchData = async () => {
+      await getSeiEcosystemAppsData()
+        .then((data) => {
+          setApps(data.data)
+          setLoading(false)
+        })
+        .catch((error) => {
+          console.error('Failed to fetch data:', error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+    fetchData()
+  }, [])
+
+  if (!apps || loading) return <div>Loading...</div>
+  const groupAppsByCategory = groupBy(apps, (app) =>
+    app.fieldData.categorie === undefined ? 'Others' : app.fieldData.categorie
+  )
+  const categories = Object.keys(groupAppsByCategory)
+
+  return (
+    <div className="flex flex-col gap-8 mt-8">
+      {categories.map((category) => {
+        return (
+          <div key={category} className="flex flex-col gap-4 border-t pt-8">
+            <h2 className="text-2xl font-semibold">{category}</h2>
+            <div className="grid grid-cols-4 gap-6">
+              {groupAppsByCategory[category].map((app, index) => {
+                const logo = app.fieldData.logo
+                return (
+                  <a
+                    href={app.fieldData.link}
+                    key={index}
+                    target="_blank"
+                    className="flex flex-col border border-gray-200 dark:border-gray-700 rounded-lg overflow-hidden hover:opacity-80 transition-all relative group"
+                  >
+                    <div className="bg-black text-white p-2 absolute rounded-md -right-8 -top-8  group-hover:top-1 group-hover:right-1 transition-all">
+                      <ExternalLinkIcon className="h-4 w-4" />
+                    </div>
+                    {logo && (
+                      <div className="flex flex-col">
+                        <img
+                          src={logo.url}
+                          alt={logo.name}
+                          className="h-full w-full aspect-square"
+                        />
+                        <div className="truncate bg-gray-100 dark:bg-gray-800 p-4 font-semibold">
+                          {app.fieldData.name}
+                        </div>
+                      </div>
+                    )}
+                  </a>
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default EcosystemMap
---
+++ components/EcosystemMap/index.ts
@@ -0,0 +1,1 @@
+export { default as EcosystemMap } from "./EcosystemMap";
