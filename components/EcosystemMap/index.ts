ff304250d1dfd6a6183e427300f7468d757e3160
partyparrotgreg <2233348+partyparrotgreg@users.noreply.github.com>
2024-06-11 11:40:12 +0100

    toc for ecosystem map

--- components/EcosystemMap/index.ts
+++ components/EcosystemMap/index.ts
@@ -1,1 +1,4 @@
-export { default as EcosystemMap } from "./EcosystemMap";
+export { default as EcosystemDynamicSection } from './EcosystemDynamicSection'
+export { default as EcosystemMap } from './EcosystemMap'
+export { default as EcosystemSection } from './EcosystemSection'
+export { default as EcosystemSkeleton } from './EcosystemSkeleton'
