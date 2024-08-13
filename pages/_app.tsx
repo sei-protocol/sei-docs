eaac00c37f6025ff289d5fc816b414729f651ca6
Anton Dosta <a.dosta@icloud.com>
2024-08-13 12:06:50 -0700

    feat: Cookbook Onboard integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '@rainbow-me/rainbowkit/styles.css';
 import {ThemeProvider} from 'next-themes';
 import '@mantine/core/styles.css';
 import {MantineWrapper} from "../components/MantineWrapper";
-
+import {AskCookbook} from "../components/AskCookbook";
 
 export default function Nextra({ Component, pageProps }) {
     return (
@@ -12,6 +12,7 @@ export default function Nextra({ Component, pageProps }) {
             <MantineWrapper>
                 <Component {...pageProps} />
             </MantineWrapper>
+            <AskCookbook />
         </ThemeProvider>
 
   );
