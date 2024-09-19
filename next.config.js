bb2638d5066685abd5f39861255e2cff1b56d5da
bt <99624770+besated@users.noreply.github.com>
2024-09-19 09:03:27 -0700

    Endpoint route fixes  (#138)
    * Fix color scheme for mantine
    
    * Add static paths and props to endpoint routes
    
    * Fix duplicate paths
    
    * Fix duplicate paths

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+	theme: 'nextra-theme-docs',
+	themeConfig: './theme.config.tsx'
 });
 
 module.exports = withNextra({
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sei.io',
-        pathname: '**',
-      }
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.sei.io',
+				pathname: '**'
+			}
+		]
+	}
 });
