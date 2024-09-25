9cc782b7419a06ee7ecbccee228bd722b449a92c
besated <bryan@seinetwork.io>
2024-09-24 18:07:54 -0700

    Remove custom image loader

--- utils/customImageLoader.ts
+++
@@ -1,9 +0,0 @@
-const customImageLoader = ({ src, width, quality }) => {
-	// Validate width and quality parameters
-	const validWidth = Number.isInteger(width) && width > 0 ? width : 800;
-	const validQuality = quality && quality > 0 && quality <= 100 ? quality : 75;
-
-	return `${src}?w=${validWidth}&q=${validQuality}`;
-};
-
-export default customImageLoader;
