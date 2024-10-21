dd864d21c3074066605d55f2a94f2fdc8a77bd38
seiyan-writer <seiyan.writer@gmail.com>
2024-10-21 02:39:53 -0400

    update beginner data + use placeholder img for logo

--- data/intermediateData.ts
+++ data/intermediateData.ts
@@ -1,65 +1,83 @@
 export const intermediateData = [
   {
     id: 1,
-    name: "Working with CosmWasm Contracts",
+    name: "1. Working with CosmWasm Contracts",
     link: "/build/cosmwasm-general",
     'short-description': "Learn how to work with CosmWasm contracts on the Sei Network.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 2,
-    name: "Working with EVM Contracts",
+    name: "2. Working with EVM Contracts",
     link: "/build/evm-general",
     'short-description': "Discover how to interact with EVM contracts on the Sei Network.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 3,
-    name: "Setting Up Multi-Sig Accounts",
+    name: "3. Setting Up Multi-Sig Accounts",
     link: "/build/multi-sig-accounts",
     'short-description': "Learn how to set up multi-sig accounts for secure operations.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 4,
-    name: "Achieving Interoperability Between VMs",
+    name: "4. Achieving Interoperability Between VMs",
     link: "/start/dev-interoperability",
     'short-description': "Guide to interoperability between VMs on the Sei Network.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 5,
-    name: "Interoperability with IBC Protocol",
+    name: "5. Interoperability with IBC Protocol",
     link: "/build/ibc-protocol",
     'short-description': "Learn how to achieve interoperability with IBC.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 6,
-    name: "Managing and Optimizing Transactions",
+    name: "6. Managing and Optimizing Transactions",
     link: "/start/dev-transactions",
     'short-description': "Learn about managing and optimizing transactions.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 7,
-    name: "Configuring a Developer Node",
+    name: "7. Configuring a Developer Node",
     link: "/node/node-configuration",
     'short-description': "Instructions on setting up and configuring a developer node.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 8,
-    name: "Advanced Querying and State Management",
+    name: "8. Advanced Querying and State Management",
     link: "/start/dev-querying-state",
     'short-description': "Guide to querying and managing state efficiently.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 9,
-    name: "Deploying and Testing Smart Contracts",
+    name: "9. Deploying and Testing Smart Contracts",
     link: "/build/smart-contract-deployment-testing",
     'short-description': "Learn how to deploy and test smart contracts effectively.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   }
 ];
