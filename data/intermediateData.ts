40094ea1659d15e328244621bffa5aa6bdc5f19e
salmad3 <danny.f.salman@gmail.com>
2024-10-21 03:00:02 -0400

    placeholders + edits

--- data/intermediateData.ts
+++ data/intermediateData.ts
@@ -1,18 +1,18 @@
 export const intermediateData = [
   {
     id: 1,
-    name: "1. Working with CosmWasm Contracts",
-    link: "/build/cosmwasm-general",
-    'short-description': "Learn how to work with CosmWasm contracts on the Sei Network.",
+    name: "1. Working with EVM Contracts",
+    link: "/build/evm-general",
+    'short-description': "Discover how to interact with EVM contracts on the Sei Network.",
     logo: {
       url: "/assets/sei_symbol_gradient.png",
     },
   },
   {
     id: 2,
-    name: "2. Working with EVM Contracts",
-    link: "/build/evm-general",
-    'short-description': "Discover how to interact with EVM contracts on the Sei Network.",
+    name: "2. Working with CosmWasm Contracts",
+    link: "/build/cosmwasm-general",
+    'short-description': "Learn how to work with CosmWasm contracts on the Sei Network.",
     logo: {
       url: "/assets/sei_symbol_gradient.png",
     },
@@ -37,16 +37,16 @@ export const intermediateData = [
   },
   {
     id: 5,
-    name: "5. Interoperability with IBC Protocol",
+    name: "5. Interoperability with IBC",
     link: "/build/ibc-protocol",
-    'short-description': "Learn how to achieve interoperability with IBC.",
+    'short-description': "Learn how to achieve interoperability with the IBC protocol.",
     logo: {
       url: "/assets/sei_symbol_gradient.png",
     },
   },
   {
     id: 6,
-    name: "6. Managing and Optimizing Transactions",
+    name: "6. Managing Transactions",
     link: "/start/dev-transactions",
     'short-description': "Learn about managing and optimizing transactions.",
     logo: {
@@ -55,7 +55,7 @@ export const intermediateData = [
   },
   {
     id: 7,
-    name: "7. Configuring a Developer Node",
+    name: "7. Configuring a Node",
     link: "/node/node-configuration",
     'short-description': "Instructions on setting up and configuring a developer node.",
     logo: {
