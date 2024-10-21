376e2945d267fb4434d741e172e34d0e06e80c73
seiyan-writer <seiyan.writer@gmail.com>
2024-10-21 03:00:02 -0400

    placeholders + edits

--- data/docsData.ts
+++ data/docsData.ts
@@ -2,42 +2,54 @@ export const docsData = [
   {
     id: 1,
     name: "foo",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
     link: "/docs/deploy-contract",
     'short-description': "Learn how to.",
   },
   {
     id: 2,
     name: "bar",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
     link: "/docs/setup-node",
     'short-description': "Learn how to.",
   },
   {
     id: 3,
     name: "baz",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
     link: "/docs/stake-tokens",
     'short-description': "Learn how to.",
   },
   {
     id: 4,
     name: "foo",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
     link: "/docs/deploy-contract",
     'short-description': "Learn how to.",
   },
   {
     id: 5,
     name: "bar",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
     link: "/docs/setup-node",
     'short-description': "Learn how to.",
   },
   {
     id: 6,
     name: "baz",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
     link: "/docs/stake-tokens",
     'short-description': "Learn how to.",
   },
