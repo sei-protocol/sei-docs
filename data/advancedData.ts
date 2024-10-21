cf120b6c4a0812fb7abd86b12e6c2a6c84d7f0a2
salmad3 <danny.f.salman@gmail.com>
2024-10-21 02:39:53 -0400

    update beginner data + use placeholder img for logo

--- data/advancedData.ts
+++ data/advancedData.ts
@@ -4,62 +4,80 @@ export const advancedData = [
     name: "foo",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 2,
     name: "bar",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 3,
     name: "baz",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 4,
     name: "foo",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 5,
     name: "bar",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 6,
     name: "baz",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 7,
     name: "foo",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 8,
     name: "bar",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
   {
     id: 9,
     name: "baz",
     link: "#",
     'short-description': "Learn how to.",
-    logo: null,
+    logo: {
+      url: "/assets/sei_symbol_gradient.png",
+    },
   },
 ];
