63d4a64eb223e7909cbdf32ba7bbf16d0a63bf63
besated <bryan@seinetwork.io>
2024-11-29 09:17:12 -0800

    Clean up intro pages

--- components/LinkCard/LinkCard.tsx
+++ components/LinkCard/LinkCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { ReactNode } from "react";
 
 interface LinkCardProps {
@@ -10,18 +11,18 @@ interface LinkCardProps {
 // TODO: Handle light mode better
 const LinkCard = ({ title, description, link, icon }: LinkCardProps) => {
 	return (
-		<a
+		<Link
 			className="bg-gradient-to-b from-primary to-primary/70 rounded-lg p-4"
 			href={link}
 		>
-			{icon && <div className="w-10 h-10 mb-2">{icon}</div>}
+			{icon && <div className="text-white mb-2">{icon}</div>}
 			<div>
 				<h3 className="text-lg font-bold text-white">{title}</h3>
 				{description && (
-					<p className="text-sm text-white/70 mt-2">{description}</p>
+					<p className="text-sm text-white/80 mt-2">{description}</p>
 				)}
 			</div>
-		</a>
+		</Link>
 	);
 };
 
