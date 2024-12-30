efc812a3a06236527e94c7a4816f14cbae0cef5c
seiyan-writer <seiyan.writer@gmail.com>
2024-12-29 22:18:29 -0500

    fix mantine integration issues and type errors in multiple components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,13 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { ColorSchemeScript } from '@mantine/core';
 
 export default function Document() {
-	return (
-		<Html lang='en'>
-			<Head>
-				<ColorSchemeScript defaultColorScheme='auto' />
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
