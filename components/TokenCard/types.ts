203e582e00c9f38a10f376014b28316f6c670099
seyian-writer <seyian.writer@gmail.com>
2025-01-03 22:20:52 -0500

    enh tokencard

--- components/TokenCard/types.ts
+++ components/TokenCard/types.ts
@@ -1,24 +1,25 @@
+// components/TokenCard/types.ts
 export interface DetailItem {
-    label: string;
-    content: string;
-  }
-  
-  export interface ReferenceGuideSection {
-    content: string;
-    command?: string;
-  }
-  
-  export interface ReferenceGuide {
-    title: string;
-    sections: ReferenceGuideSection[];
-  }
-  
-  export type DetailItemOrReferenceGuide = DetailItem | ReferenceGuide;
-  
-  export interface TokenCardProps {
-    title: string;
-    description?: string;
-    tooltip?: string;
-    details: DetailItemOrReferenceGuide[];
-    referenceGuides?: ReferenceGuide[];
-  }
+  label: string;
+  content: string;
+}
+
+export interface ReferenceGuideSection {
+  content: string;
+  command?: string;
+}
+
+export interface ReferenceGuide {
+  title: string;
+  sections: ReferenceGuideSection[];
+}
+
+export type DetailItemOrReferenceGuide = DetailItem | ReferenceGuide;
+
+export interface TokenCardProps {
+  title: string;
+  description?: string;
+  tooltip?: string;
+  details: DetailItemOrReferenceGuide[];
+  referenceGuides?: ReferenceGuide[];
+}
