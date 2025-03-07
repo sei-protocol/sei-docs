d28e373b9d2f4b24f565d6ac3ff349f3251b5a6a
Carson <104383295+codebycarson@users.noreply.github.com>
2025-03-06 23:08:50 -0700

    Added more metadata using the NextJS metadata API

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { getPageMap } from 'nextra/page-map';
+import { Toaster } from 'sonner';
+import { Metadata } from 'next';
 
 import DocsProviders from '../src/providers/DocsProviders';
 
@@ -8,23 +10,69 @@ import '@radix-ui/themes/styles.css';
 import 'nextra-theme-docs/style.css';
 import './globals.css';
 
-import { Toaster } from 'sonner';
+export const metadata: Metadata = {
+	title: {
+		default: 'Sei Docs',
+		template: '%s | Sei Docs'
+	},
+	description: 'Documentation for Sei Network',
+	category: 'technology',
+	openGraph: {
+		url: 'https://docs.sei.io',
+		siteName: 'Sei Docs',
+		locale: 'en_US',
+		images: [
+			{
+				url: 'https://www.docs.sei.io/assets/sei-v2-banner.jpg',
+				alt: 'Sei V2 Overview'
+			}
+		],
+		type: 'website'
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Sei Docs',
+		description: 'Documentation for Sei Network',
+		creator: '@SeiNetwork',
+		images: ['https://www.docs.sei.io/assets/sei-v2-banner.jpg']
+	},
+	referrer: 'origin-when-cross-origin',
+	keywords: ['Sei', 'Sei Network', 'Sei Blockchain', 'Sei Docs', 'Sei Documentation', 'EVM', 'ERC20', 'ERC721'],
+	creator: 'Sei Network',
+	publisher: 'Sei Network',
+	formatDetection: {
+		email: false,
+		address: false,
+		telephone: false
+	},
+	robots: {
+		index: true,
+		follow: true,
+		nocache: false,
+		googleBot: {
+			index: true,
+			follow: true,
+			noimageindex: false,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1
+		}
+	},
+	icons: {
+		icon: '/icon.png',
+		shortcut: '/shortcut-icon.png',
+		apple: '/apple-icon.png',
+		other: {
+			rel: 'apple-touch-icon-precomposed',
+			url: '/apple-touch-icon-precomposed.png'
+		}
+	}
+};
 
 export default async function RootLayout({ children }) {
 	return (
 		<html lang='en' dir='ltr' suppressHydrationWarning style={{ width: '100%', height: '100%' }}>
-			<head>
-				<meta charSet='UTF-8' />
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-
-				<link rel='icon' type='image/x-icon' href='/favicon.ico' />
-				<link rel='canonical' href='https://docs.sei.io' />
-
-				<meta name='keywords' content='Sei, Sei Network, Sei Blockchain, Sei Docs, Sei Documentation' />
-				<meta name='author' content='Sei Network' />
-			</head>
-
-			<body style={{ width: '100%', height: '100%' }} data-pagefind-body>
+			<body style={{ width: '100%', height: '100%' }}>
 				<Toaster position='bottom-left' />
 				<DocsProviders pageMap={await getPageMap()}>{children}</DocsProviders>
 				<script id='ze-snippet' src='https://static.zdassets.com/ekr/snippet.js?key=95ec0096-4a77-48ad-b645-f010d3cb8971' />
