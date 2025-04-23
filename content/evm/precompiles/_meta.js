40d638670a6d1374207eb37c5291fc53116b6577
alexander-sei <alexander@seifdn.org>
2025-04-23 12:46:27 +0200

    Split up precompiles

--- content/evm/precompiles/_meta.js
+++ content/evm/precompiles/_meta.js
@@ -1,14 +1,8 @@
 export default {
 	'example-usage': 'Example Usage',
-	addr: 'Address',
-	bank: 'Bank',
-	cosmwasm: 'CosmWasm',
 	distribution: 'Distribution',
 	governance: 'Governance',
-	ibc: 'IBC',
 	json: 'JSON',
 	oracle: 'Oracle',
-	pointer: 'Pointer',
-	pointerview: 'Pointerview',
 	staking: 'Staking'
 };
