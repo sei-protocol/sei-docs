b4c990ae89e43444c6d08dce6d415ba3ed8453c1
carson <carson.aberle+codebycarson@gmail.com>
2025-05-29 15:03:58 -0700

    Switched bundled assets to CDN versions

--- src/components/ImageWithCaption/ImageWithCaption.tsx
+++ src/components/ImageWithCaption/ImageWithCaption.tsx
@@ -2,10 +2,12 @@ import Image, { StaticImageData } from "next/image";
 import { twMerge } from "tailwind-merge";
 
 interface ImageWithCaption {
-  img: StaticImageData;
+  img: StaticImageData | string;
   alt: string;
   caption?: string;
   className?: string;
+  width?: number;
+  height?: number;
 }
 
 export default function ImageWithCaption({
@@ -13,7 +15,12 @@ export default function ImageWithCaption({
   alt,
   caption,
   className,
+  width,
+  height,
 }: ImageWithCaption) {
+  // If img is a string (CDN URL), we need to provide width/height or use fill
+  const isStringUrl = typeof img === 'string';
+  
   return (
     <div
       className={twMerge(
@@ -21,7 +28,17 @@ export default function ImageWithCaption({
         className
       )}
     >
-      <Image src={img} alt={alt} />
+      {isStringUrl ? (
+        <Image 
+          src={img} 
+          alt={alt} 
+          width={width || 800} 
+          height={height || 600}
+          style={{ width: 'auto', height: 'auto' }}
+        />
+      ) : (
+        <Image src={img} alt={alt} />
+      )}
       {caption && <p className="text-gray-400">{caption}</p>}
     </div>
   );
