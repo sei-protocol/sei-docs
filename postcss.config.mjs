3e7066170e6c8d3a0b5462721f393b4301f88839
alexander-sei <alexander@seifdn.org>
2025-11-08 16:59:25 +0100

    Add more optimizations

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,26 @@ const config = {
 	plugins: {
 		'@tailwindcss/postcss': {},
 		autoprefixer: {},
-		...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: ['default', { discardComments: { removeAll: true } }] } } : {})
+		...(process.env.NODE_ENV === 'production'
+			? {
+					cssnano: {
+						preset: [
+							'default',
+							{
+								discardComments: { removeAll: true },
+								reduceIdents: true,
+								mergeLonghand: true,
+								mergeRules: true,
+								minifyFontValues: true,
+								minifyGradients: true,
+								normalizeWhitespace: true,
+								colormin: true,
+								calc: false
+							}
+						]
+					}
+				}
+			: {})
 	}
 };
 
