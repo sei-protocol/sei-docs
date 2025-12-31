28005a24f736f0765fae98c0ec62299b5842fd9b
alexander-sei <alexander@seifdn.org>
2025-12-31 01:54:39 +0100

    Reduce cosmos-sdk section

--- content/cosmos-sdk/_meta.js
+++ content/cosmos-sdk/_meta.js
@@ -2,22 +2,7 @@ export default {
 	index: {
 		title: 'Home'
 	},
-	'-- Essentials': {
-		type: 'separator',
-		title: 'Essentials'
-	},
 	networks: 'Network Information',
-
-	'-- Transactions': {
-		type: 'separator',
-		title: 'Transactions'
-	},
 	transactions: 'Transaction Overview',
-	'execute-multiple': 'Executing Multiple Transactions',
-
-	'-- Modules': {
-		type: 'separator',
-		title: 'Modules'
-	},
 	'multi-sig-accounts': 'Multi-Sig Accounts'
 };
