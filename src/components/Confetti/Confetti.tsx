642219a1706040f55326fe8935934edae8360d8d
alexander-sei <alexander@seifdn.org>
2025-12-31 18:40:23 +0100

    Improve confetti

--- src/components/Confetti/Confetti.tsx
+++ src/components/Confetti/Confetti.tsx
@@ -1,33 +1,19 @@
 'use client';
 
-import React from 'react';
 import ReactConfetti from 'react-confetti';
 
 export const Confetti = () => {
-	// We need window size for react-confetti to work properly full screen
-	// If react-use is not available we might need a custom hook or just 100vw/100vh
-	// Let's check if react-use is installed, otherwise fallback to CSS approach or basic hook
-
-	const [dimensions, setDimensions] = React.useState({ width: 0, height: 0 });
-
-	React.useEffect(() => {
-		const updateDimensions = () => {
-			setDimensions({
-				width: window.innerWidth,
-				height: window.innerHeight
-			});
-		};
-
-		// Initial set
-		updateDimensions();
-
-		window.addEventListener('resize', updateDimensions);
-		return () => window.removeEventListener('resize', updateDimensions);
-	}, []);
-
 	return (
-		<div className='fixed inset-0 w-screen h-screen z-0 pointer-events-none'>
-			<ReactConfetti width={dimensions.width} height={dimensions.height} numberOfPieces={50} recycle={true} />
+		<div className='fixed inset-0 w-screen h-screen z-40 pointer-events-none'>
+			<ReactConfetti
+				style={{
+					position: 'absolute',
+					width: '100%',
+					height: '100%'
+				}}
+				numberOfPieces={50}
+				recycle={true}
+			/>
 		</div>
 	);
 };
