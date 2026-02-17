0cde42375d151bf587d9958630ef612be212e38d
besated <bryan@seinetwork.io>
2026-02-17 11:48:24 -0800

    Replace ga with gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { getPageMap } from 'nextra/page-map';
 import { Toaster } from 'sonner';
 import { Metadata } from 'next';
-import Script from 'next/script';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 import DocsProviders from '../src/providers/DocsProviders';
 
@@ -105,19 +105,20 @@ export default async function RootLayout({ children }) {
 				/>
 			</head>
 			<body style={{ width: '100%', height: '100%' }}>
+				{/* Google Tag Manager (noscript) fallback */}
+				<noscript>
+					<iframe
+						src='https://www.googletagmanager.com/ns.html?id=GTM-KKN784SR'
+						height='0'
+						width='0'
+						style={{ display: 'none', visibility: 'hidden' }}
+						title='Google Tag Manager'
+					/>
+				</noscript>
 				<Toaster position='bottom-left' />
 				<DocsProviders pageMap={await getPageMap()}>{children}</DocsProviders>
 			</body>
-			{/* Google Analytics: lazy-load after window load */}
-			<Script src='https://www.googletagmanager.com/gtag/js?id=G-G33FDB53X5' strategy='lazyOnload' />
-			<Script id='ga-init' strategy='lazyOnload'>
-				{`
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
-					gtag('config', 'G-G33FDB53X5', { anonymize_ip: true });
-				`}
-			</Script>
+			<GoogleTagManager gtmId='GTM-KKN784SR' />
 		</html>
 	);
 }
